06ff6df7ac1dc5ad4fe45f3204fbbb8a5df5b309
Gerald Ventura <ventura.c.gerald@gmail.com>
2020-04-25 00:49:20 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -9,9 +9,9 @@ class EmailAddressParser
  def parse
    csv_emails.split.collect do |address|
      address.split(',')
-   end
+   end.uniq
   #  .uniq
-end.uniq
+end
 end
 
 emails1 = "asd@qw.com, per@er.org, Zaza@sugar.net"
